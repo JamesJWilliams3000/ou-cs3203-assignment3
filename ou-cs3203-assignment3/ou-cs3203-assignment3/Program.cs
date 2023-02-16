0a201b1f90dad39dd18b28d09626fa07fd62d131
James J Williams <112959298+JamesJWilliams3000@users.noreply.github.com>
2023-02-16 15:51:28 -0600

    part 1

---
+++ ou-cs3203-assignment3/ou-cs3203-assignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//james williams
+//
+
+namespace ou_cs3203_assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum is: " + Sum(args));
+            Console.ReadKey();
+        }
+
+        private static int Sum(string[] args)
+        {
+            int sum = 0;
+            foreach (var arg in args)
+            {
+                //insert error checking here
+                int num = Int32.Parse(arg);
+                sum += num;
+            }
+
+            return sum;
+        }
+    }
+}
